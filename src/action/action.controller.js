a70fb520068c6482c62a9a736d51a23e378fcfca
Julius Morkūnas <julius@morkunas.net>
2019-02-23 04:47:48 -0800

    Do not allow for the same player to make an action twice in a row

--- src/action/action.controller.js
+++ src/action/action.controller.js
@@ -6,17 +6,19 @@ module.exports.list = async (req, res) => {
 };
 
 module.exports.add = async (req, res) => {
-  const existingActions = await Action.find({ row: req.body.row, cell: req.body.cell });
-  if (!existingActions.length) {
-    const action = req.body.timestamp
-      ? new Action(req.body)
-      : new Action({ ...req.body, timestamp: +new Date() });
-    await action.save();
-    res.json(action);
-    return;
+  const existingActionInSameCell = await Action.findOne({ row: req.body.row, cell: req.body.cell });
+  if (existingActionInSameCell) {
+    throw new Error('Action for this location already exist');
   }
-  // TODO: Check if previous action wasn't by the same player
-  throw new Error('Action for this location already exist');
+  const previousAction = await Action.findOne().sort({ _id: -1 });
+  if (previousAction && previousAction.player === req.body.player) {
+    throw new Error('Same player cannot perform an action twice in a row');
+  }
+  const action = req.body.timestamp
+    ? new Action(req.body)
+    : new Action({ ...req.body, timestamp: +new Date() });
+  await action.save();
+  res.json(action);
 };
 
 module.exports.clear = async (req, res) => {
